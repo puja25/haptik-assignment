8cdfcd522b4eceacc7860d93afdcc11482835300
puja25 <pujadeshpande25@gmail.com>
2021-11-12 12:10:23 +0530

    initial commit

---
+++ src/Components/List/index.jsx
@@ -0,0 +1,195 @@
+import React, { useState } from "react";
+import "./index.css";
+
+const ListUI = ({
+  list,
+  onDeleteName,
+  favouriteList,
+  favList,
+  RemoveFromList,
+}) => {
+  return (
+    <>
+      {favList.map((val, index) => {
+        return (
+          <div className="list-wrapper" key={index}>
+            <section>
+              <h4>{val.name}</h4>
+              <span>{val.status}</span>
+            </section>
+            <section className="icon-alignment">
+              <i
+                className={`fa fa-star-o ${val.sortIcon ? " sort-color" : ""}`}
+                id={index}
+                onClick={RemoveFromList}
+              ></i>
+            </section>
+            <section className="icon-alignment">
+              <i
+                className="fa fa-trash-o"
+                onClick={onDeleteName}
+                id={index}
+              ></i>
+            </section>
+          </div>
+        );
+      })}
+      {list.map((val, index) => {
+        return (
+          <div className="list-wrapper" key={index}>
+            <section>
+              <h4>{val.name}</h4>
+              <span>{val.status}</span>
+            </section>
+            <section className="icon-alignment">
+              <i
+                className={`fa fa-star-o ${val.sortIcon ? " sort-color" : ""}`}
+                id={index}
+                onClick={favouriteList}
+              ></i>
+            </section>
+            <section className="icon-alignment">
+              <i
+                className="fa fa-trash-o"
+                onClick={onDeleteName}
+                id={index}
+              ></i>
+            </section>
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+const List = () => {
+  const [list, updateList] = useState([
+    { name: "Rahul", status: "is your friend", sortIcon: false },
+  ]);
+  const [userName, updateUserName] = useState({
+    name: "",
+    status: "is your friend",
+  });
+  const [favList, updateFavList] = useState([]);
+  const [error, setError] = useState(false);
+  const [searchIcon, setSearchIcon] = useState(false);
+  const [searchName, setSearchName] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const listPerPage = 4;
+
+  const indexOfLastTodo = currentPage * listPerPage;
+  const indexOfFirstTodo = indexOfLastTodo - listPerPage;
+  const lists = list.slice(indexOfFirstTodo, indexOfLastTodo);
+
+  const pageNumbers = [];
+  for (let i = 1; i <= Math.ceil(list.length / listPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
+  const handleClick = (event) => {
+    setCurrentPage(event.target.id);
+  };
+  const addFriendList = (event) => {
+    if (event.key === "Enter" && event.target.value === "") {
+      setError(true);
+    } else if (
+      event.key === "Enter" &&
+      event.target.value !== "" &&
+      event.target.name === "name"
+    ) {
+      updateList([...list, userName]);
+    } else if (
+      event.key === "Enter" &&
+      event.target.value !== "" &&
+      event.target.name === "searchName"
+    ) {
+      const filterList = list.find((element) => element.name === searchName);
+      updateList([filterList]);
+    }
+  };
+  const onChangeName = (event) => {
+    if (event.target.name === "searchName") {
+      setSearchName(event.target.value);
+    } else {
+      let data = {
+        ...userName,
+        [event.target.name]: event.target.value,
+      };
+      updateUserName(data);
+    }
+  };
+  const onDeleteName = (event) => {
+    let r = window.confirm("Are you sure you want to delete a name?");
+    if (r == true) {
+      let test = [...list];
+      test.splice(event.target.id, 1);
+      updateList(test);
+    }
+  };
+
+  const searchOpen = () => setSearchIcon(!searchIcon);
+
+  const favouriteList = (event) => {
+    list[event.target.id].sortIcon = !list[event.target.id].sortIcon;
+    if (list[event.target.id].sortIcon === true) {
+      updateFavList([...favList, list[event.target.id]]);
+      let test = [...list];
+      test.splice(event.target.id, 1);
+      updateList(test);
+    }
+  };
+
+  const RemoveFromList = (event) => {
+    favList[event.target.id].sortIcon = !favList[event.target.id].sortIcon;
+    updateList([...list, favList[event.target.id]]);
+    let test = [...favList];
+    test.splice(event.target.id, 1);
+    updateFavList(test);
+  };
+
+  const renderPageNumbers =
+    pageNumbers.length > 1 &&
+    pageNumbers.map((number) => {
+      return (
+        <li key={number} id={number} onClick={handleClick}>
+          {number}
+        </li>
+      );
+    });
+  return (
+    <>
+      <i className="fa fa-search search-alignment" onClick={searchOpen}></i>
+      {searchIcon ? (
+        <input
+          type="search"
+          placeholder="Search name"
+          className={`input-alignment ${error ? " error-border" : ""}`}
+          value={searchName}
+          onKeyDown={addFriendList}
+          onChange={onChangeName}
+          name="searchName"
+        />
+      ) : (
+        <input
+          type="search"
+          placeholder="Enter your friend's name"
+          className={`input-alignment ${error ? " error-border" : ""}`}
+          value={userName.name}
+          onKeyDown={addFriendList}
+          onChange={onChangeName}
+          name="name"
+        />
+      )}
+      <ListUI
+        list={lists}
+        onDeleteName={onDeleteName}
+        favouriteList={favouriteList}
+        favList={favList}
+        RemoveFromList={RemoveFromList}
+      />
+      {pageNumbers.length > 1 && <ul id="page-numbers">{renderPageNumbers}</ul>}
+    </>
+  );
+};
+
+export default List;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import ListWrapper from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ListWrapper />
   </React.StrictMode>,
   document.getElementById('root')
 );
